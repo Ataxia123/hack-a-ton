92acb2bc1403bb3653034b302b894c49389b0e8e
Georgi Dimitrov <georgijd92@gmail.com>
2024-02-07 10:11:05 -0500

    a msgs

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -8,25 +8,16 @@ const Home: NextPage = () => {
       <div className="flex items-center flex-col flex-grow pt-10">
         <div className="px-5">
           <h1 className="text-center mb-8">
-            <span className="block text-2xl mb-2">Welcome to</span>
-            <span className="block text-4xl font-bold">Scaffold-ETH 2</span>
+            <span className="block text-2xl mb-2">Welcome to the</span>
+            <span className="block text-4xl font-bold">NERD COHERENCE TESTING LAB</span>
           </h1>
           <p className="text-center text-lg">
-            Get started by editing{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              packages/nextjs/app/page.tsx
-            </code>
+            This is a testing lab for the NERD COHERENCE project. <br />
+            The purpose of this project is to test the coherence of the kEngram data.
           </p>
-          <p className="text-center text-lg">
-            Edit your smart contract{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              YourContract.sol
-            </code>{" "}
-            in{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              packages/hardhat/contracts
-            </code>
-          </p>
+          <div className="h-[150px] w-[150px] border-2 rounded-full animate-pulse backdrop-hue-rotate-90 text-center cursor-pointer">
+            GO
+          </div>
         </div>
 
         <div className="flex-grow bg-base-300 w-full mt-16 px-8 py-12">
@@ -34,9 +25,9 @@ const Home: NextPage = () => {
             <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
               <BugAntIcon className="h-8 w-8 fill-secondary" />
               <p>
-                Tinker with your smart contract using the{" "}
+                Upload your kEngram data in the{" "}
                 <Link href="/debug" passHref className="link">
-                  Debug Contract
+                  kEngram Page
                 </Link>{" "}
                 tab.
               </p>
@@ -44,9 +35,9 @@ const Home: NextPage = () => {
             <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
               <MagnifyingGlassIcon className="h-8 w-8 fill-secondary" />
               <p>
-                Explore your local transactions with the{" "}
+                Talk to the hacker Brain{" "}
                 <Link href="/blockexplorer" passHref className="link">
-                  Block Explorer
+                  Brain
                 </Link>{" "}
                 tab.
               </p>
